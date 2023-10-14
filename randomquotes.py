a63eeb7363fd4410b9f4716d7af2c563d71d33b0
Nicolas Ramirez <nram.vel94@gmail.com>
2023-10-14 10:47:11 -0500

    Initial Commit

---
+++ randomquotes.py
@@ -0,0 +1,22 @@
+from flask import Flask, jsonify
+import random
+
+app = Flask(__name__)
+
+phrases = [
+    "Get ready to be inspired…", 
+    "See rejection as redirection.",
+    "There is beauty in simplicity.",
+    "You can’t be late until you show up.",
+    "Maybe life is testing you. Don’t give up.",
+    "Impossible is just an opinion.",
+    "Alone or not you gonna walk forward.",
+]
+
+@app.route('/')
+def get_random_quote():
+    return jsonify(random.choice(phrases))
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=80)
+
